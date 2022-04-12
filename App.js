fedc69edebf2313ee3688c4cfb7058908ac4413d
zeinab-zeitoun <zeinabzeitoun.98@gmail.com>
2022-04-12 13:23:42 +1000

    drawer navigator demo

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native'
+import WelcomeScreen from './screens/WelcomeScreen'
+import UserScreen from './screens/UserScreen'
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+const Drawer = createDrawerNavigator(); 
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <NavigationContainer>
+    <Drawer.Navigator initialRouteName='User'>
+      <Drawer.Screen name="Welcome" component={WelcomeScreen}/>
+      <Drawer.Screen name="User" component={UserScreen}/>
+    </Drawer.Navigator> 
+  </NavigationContainer>;
 }
 
 const styles = StyleSheet.create({
